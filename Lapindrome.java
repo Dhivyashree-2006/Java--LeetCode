3fc0af9bcbba836ac1b8864651c28a7cd0e331d1
Dhivya Shree S <dhiviyashree2006@gmail.com>
2026-06-03 23:15:12 +0530

    Implement Lapindrome checker in Lapindrome.java

---
+++ Lapindrome.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            String s = sc.next();
+
+            int n = s.length();
+            String left, right;
+
+            if (n % 2 == 0) {
+                left = s.substring(0, n / 2);
+                right = s.substring(n / 2);
+            } else {
+                left = s.substring(0, n / 2);
+                right = s.substring(n / 2 + 1);
+            }
+
+            char[] a = left.toCharArray();
+            char[] b = right.toCharArray();
+
+            Arrays.sort(a);
+            Arrays.sort(b);
+
+            if (Arrays.equals(a, b))
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+
+        sc.close();
+    }
+}
